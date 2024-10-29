f890c85b1c4e38475548cf4171a2b5a32d53f23b
prince <sonykumarprince460@gmail.com>
2024-10-29 23:39:21 +0530

    two day of dsa

--- star-pattern/index.js
+++ star-pattern/index.js
@@ -42,6 +42,66 @@ function pattern_05(n) {
 }
 
 
+function pattern_06(n) {
+    for (let o = 1; o <= n; o++) {
+        const star = "* ".repeat(n-o)
+        console.log(star);
+    }
+}
+function pattern_07(n) {
+    for (let o = 0; o  < n; o++) {
+        let start = Array.from({length:n-o},(_,index)=>{ return index+1});
+        console.log(start.join(' '))
+    }
+}
+function pattern_08(n) {
+    for (let o = 0; o  < n; o++) {
+       let star = "";
+       let space = " ".repeat(n-o+1)
+       star += space + "* ".repeat(o+1);
+       console.log(star)
+    }
+}
+function pattern_09(n) {
+    for (let o = 0; o  < n; o++) {
+       let star = "";
+       let space = " ".repeat(o)
+       star += space + "* ".repeat(n-o);
+       console.log(star)
+    }
+}
+function pattern_10(n) {
+    // Upper part of the pattern
+    for (let o = 0; o < n; o++) {
+        let star = "";
+        let space = " ".repeat(n - o - 1); // Corrected space calculation
+        star += space + "* ".repeat(o + 1).trim(); // Trim to remove trailing space
+        console.log(star);
+    }
+    // Lower part of the pattern
+    for (let o = 0; o < n; o++) {
+        let star = "";
+        let space = " ".repeat(o);
+        star += space + "* ".repeat(n - o).trim(); // Trim to remove trailing space
+        console.log(star);
+    }
+}
+
+function pattern_11(n) {
+    for (let o = 0; o  < n; o++) {
+      let star = "*".repeat(o);
+      console.log(star)
+    }
+    for (let o = n;  o>0; o--) {
+        let star = "*".repeat(o); // 
+      
+        console.log(star)
+      }
+    
+}
+
+
+
 
 
 
@@ -53,7 +113,7 @@ function pattern_05(n) {
 function print_start() {
     console.time()
     for (let index = 0; index < 4; index++) {
-        pattern_05(2 * index + 4);
+        pattern_11(2 * index + 4);
     }
     console.timeEnd()
 } 
