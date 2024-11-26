f9eaecca965e0e1be507b3ee8b9c64314c7389bd
prince <sonykumarprince460@gmail.com>
2024-11-26 12:14:06 +0530

    day of 2 in string problem

--- string-solve/index.js
+++ string-solve/index.js
@@ -20,4 +20,46 @@ function reverseWords(str) {
   let inputString = "A man a plan a canal Panama";
   let isPalindromic = isPalindrome(inputString);
   console.log(isPalindromic);  // Output: true
+
+
+  function removeOuterParentheses(str) {
+    return str.replace(/^\(.*\)$/, '');
+  }
   
+  function removeOuterParentheses(str) {
+    return str.replace(/^\((.*)\)$/, '$1');
+  }
+  
+  // console.log(removeOuterParentheses("(Hello World)"));  // Output: "Hello World"
+  // console.log(removeOuterParentheses("(123)"));  // Output: "123"
+  
+
+  function largestOddNumber(str) {
+ /// first check only digit 
+ let num_didgit = str.match(/\d+/g)
+ let odd_digit = num_didgit.map((num)=>parseInt(num,10)
+).filter((item)=>item%2!==0)
+return Math.max.apply({},odd_digit)
+  }
+  
+  // // Example usage:
+  // let str = "abc122def45gh7ij";
+  // console.log(largestOddNumber(str));  // Output: 123
+
+  let str = ["flower","flow","flight"]
+
+  function find_prefic(str){
+    let prefix = str[0]
+
+    for (let i = 0; i < str.length; i++) {
+      
+      while(str[i].indexOf(prefix)!==0){
+        prefix = prefix.slice(0,str.length-1)
+      }
+      
+    }
+    return prefix
+  }
+  
+  let prefix_01 = find_prefic(str)
+  console.log(prefix_01)
